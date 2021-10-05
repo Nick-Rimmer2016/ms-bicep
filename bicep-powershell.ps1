4c4755b8e408b44a56ad55aaa0ceed2841b22cd4
Nick Rimmer <nick.rimmer.2009@gmail.com>
2021-10-05 09:04:03 +0100

    Add Az CLI commands

---
+++ bicep-powershell.ps1
@@ -0,0 +1,19 @@
+# Method 1 PowerShell
+$context = Get-AzSubscription -SubscriptionName 'Pay-As-You-Go'
+Set-AzContext $context
+
+Get-AzSubscription
+
+Set-AzDefault -ResourceGroupName ATA
+New-AzResourceGroupDeployment -TemplateFile main.bicep
+
+Get-AzResourceGroupDeployment -ResourceGroupName ATA | Format-Table
+
+# Method 2 - Azure CLI
+az login
+az account set --subscription "Pay-As-You-Go"
+az account list --refresh --query "[?contains(name, 'Pay-As-You-Go')].id" --output table
+az account set --subscription 19b3488b-1b19-4127-8f27-8cfb2b594615
+az configure --defaults group=ATA
+
+az deployment group create --template-file main.bicep
